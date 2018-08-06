1cf3669ec7fffceba9b96216fd681125bcef5cd4
julia <julia.ignatova11@gmail.com>
2018-08-06 17:23:13 +0300

    3. Delegated Transfer

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -7,13 +7,15 @@ contract DappToken{
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
 
+    mapping(address => mapping(address => uint256)) public allowance;
+
     //Name
     string public name = 'Dapp Token';
 
     //symbol
     string public symbol = 'DAPP';
 
-    //standart (not erc-20 standart)
+    //standart (not erc-20 requirement)
     string public standart = 'DAPP Token v1.0';
 
     event Transfer(
@@ -21,6 +23,14 @@ contract DappToken{
         address indexed _to,
         uint256 _value
     );
+
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+
+
 
     constructor(uint256 _initialSupply) public{
         //allocate the initial supply
@@ -45,5 +55,42 @@ contract DappToken{
         return true;
     }
 
+    //Delegated Transfer
+
+    //approve function
+    function approve(address _spender, uint256 _value) public returns(bool success){
+
+        //handle the allowance
+        allowance[msg.sender][_spender] = _value;
+
+        //handle approve event
+        emit Approval(msg.sender, _spender, _value);
+
+        return true;
+    }
+
+    //transferFrom
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success){
+
+        //require _from account has enough tokens
+        require(balanceOf[_from] >= _value);
+
+        //require allowance is big enough
+        require(_value <= allowance[_from][msg.sender]);
+
+        //change the balance
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        //update the allowance
+        allowance[_from][msg.sender] -= _value;
+
+        //Transfer event
+        emit Transfer(_from, _to, _value);
+
+        //return a boolean
+        return true;
+
+    }
 
 }
